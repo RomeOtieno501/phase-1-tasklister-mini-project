9d785d45846a0ef36d9b9ed400397d14dad55038
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-10-06 18:22:59 +0300

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+
+// Listening to submission
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  // Get the value of the input field
+  const taskInput = document.getElementById('new-task-description').value;
+
+  // new lis element
+  const newTask = document.createElement('li');
+  newTask.textContent = taskInput;
+
+  // Create delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Ondoa';
+  
+  // When button is clicked
+  deleteButton.addEventListener('click', function() {
+    taskList.removeChild(newTask);
+  });
+
+  // Add the delete button to the end of the element
+  newTask.appendChild(deleteButton);
+
+  // Append the new list element to the existing element
+  taskList.appendChild(newTask);
+
+  // Clear the input field
+  document.getElementById('new-task-description').value = '';
+});
+
 });
